305398ba93c8fc56532f92d29e81f5d72e4aa401
Daniel Komolafe <komozy2000@gmail.com>
2022-03-22 09:51:06 +0100

    finetuned firebase upload func, smart response, errors

--- helpers/firebase_upload.js
+++ helpers/firebase_upload.js
@@ -1,10 +1,17 @@
 const firebase = require("../config/firebase.db");
-const storage = require("firebase/storage");
+const {
+	getStorage,
+	ref,
+	uploadBytesResumable,
+	getDownloadURL,
+} = require("firebase/storage");
 
-const myStorage = storage.getStorage(firebase);
+const storage = getStorage(firebase);
 
-exports.firebaseUpload = async (file) => {
-	try {
+storage.maxUploadRetryTime = 30000;
+
+exports.firebaseUpload = (file) => {
+	return new Promise((resolve, reject) => {
 		// Add Image to Storage and return the file path
 		// Grab the file
 		const imgFile = file;
@@ -17,19 +24,62 @@ exports.firebaseUpload = async (file) => {
 		const fileName = `${name}_${timestamp}.${type}`;
 
 		// Create a URL and return the storage reference
-		const newPhotoRef = storage.ref(myStorage, fileName);
+		const storageRef = ref(storage, fileName);
 
-		await storage.uploadBytes(
-			newPhotoRef,
+		const uploadTask = uploadBytesResumable(
+			storageRef,
 			imgFile.buffer,
 			imgFile.mimetype
 		);
 
-		const downloadURL = await storage.getDownloadURL(newPhotoRef);
+		/** Register three observers:
+		 * 1. 'state_changed' observer, called any time the state changes
+		 * 2. Error observer, called on failure
+		 * 3. Completion observer, called on successful completion
+		 */
 
-		return downloadURL;
-	} catch (err) {
-		console.error(err);
-		return [err, "Failed to upload to firebase!"];
-	}
+		uploadTask.on(
+			"state_changed",
+			(snapshot) => {
+				// Observe state change events such as progress, pause, and resume
+				// Get task progress, number of bytes uploaded/total number of bytes to be uploaded
+				const progress =
+					(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+				console.log(`Upload is ${progress}% done`);
+
+				switch (snapshot.state) {
+					case "paused":
+						console.log("Upload is paused");
+						break;
+					case "running":
+						console.log("Upload is running");
+						break;
+				}
+			},
+			(error) => {
+				switch (error.code) {
+					case "storage/retry-limit-exceeded":
+						reject(
+							"Maximum time limit exceeded. Try uploading again."
+						);
+					case "storage/unauthorized":
+						reject(
+							"User doesn't have permission to access the object"
+						);
+					case "storage/canceled":
+						reject("User canceled the upload");
+					case "storage/unknown":
+						reject(
+							"Unknown error occurred, inspect error.serverResponse"
+						);
+				}
+			},
+			() => {
+				// Upload completed successfully, now we can get the download URL
+				getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+					resolve(downloadURL);
+				});
+			}
+		);
+	});
 };
