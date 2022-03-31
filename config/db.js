b1fe4a54e63a98ff82b891fded88add6845c8a3e
Daniel Komolafe <komozy2000@gmail.com>
2022-03-31 16:29:50 +0100

    cloud db uri for production

--- config/db.js
+++ config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-const uri = process.env.MONGO_LOCAL;
+const uri =
+	process.env.NODE_ENV === "production"
+		? process.env.MONGO_CLOUD
+		: process.env.MONGO_LOCAL;
 
 exports.connectDB = async () => {
 	await mongoose
